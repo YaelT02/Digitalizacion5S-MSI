543d786c687a44393cc4f9d3ae96e79bfc480fca
SERGIO YAEL TREJO MAYA <sergioytrejo02@hotmail.com>
2024-02-14 17:22:11 -0600

    Cambios 14-Feb-2024

---
+++ Digitalizacion5S/Digitalizacion5S/Model/HallazgosOOModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digitalizacion5S.Model
+{
+    internal class HallazgosOOModel
+    {
+    }
+}
---
+++ Digitalizacion5S/Digitalizacion5S/ViewModel/HallazgosOOViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Digitalizacion5S.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Digitalizacion5S.ViewModel
+{
+    public class HallazgosOOViewModel : BaseViewModel
+    {
+        FirebaseHelper firebaseHelper = new FirebaseHelper();
+
+        #region Attributes
+
+        #endregion
+
+        #region Proporties
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+    }
+}
